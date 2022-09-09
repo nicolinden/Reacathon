fb4f87e19b119412e16f65b99aacefcf455ddc30
Ted Castelijns <ted.castelijns@expertum.net>
2022-09-09 11:53:55 +0200

    OK

--- web/src/components/Launch/Launch.tsx
+++ web/src/components/Launch/Launch.tsx
@@ -9,20 +9,20 @@ interface LaunchProps{
 export const Launch = (props:LaunchProps) => {
 
     const successIcon = props.success ?
-    <TickCircleIcon size={40} color="success" marginLeft={16} marginRight={16} />
-     : <BanCircleIcon size={40} color="danger" marginLeft={16} marginRight={16} />;
+    <TickCircleIcon size={30} color="success" marginTop={5} marginLeft={20} marginRight={10} />
+     : <BanCircleIcon size={30} color="danger" marginTop={5} marginLeft={20} marginRight={10} />;
 
   return (
     <Pane className="launchWrapper">
-        <img src={props.icon} alt={props.icon} width="70" height="70" className="launchItemPart"/>
+        <img src={props.icon} alt={props.icon} width="50" height="50" className="launchItemPart"/>
         <div className="launchItemPart">
-            <div>{props.title}</div>
-            <div>{props.date}</div>
+            <div className="textLaunchItem">{props.title}</div>
+            <div className="dateLaunchItem">{props.date}</div>
         </div>
-        <div className="launchItemPart">{successIcon}</div>
         <div className="launchItemLastPart">
             <ChevronRightIcon size={40}/>
         </div>
+        <div className="launchItemLastPart">{successIcon}</div>
     </Pane>
   );
 };
